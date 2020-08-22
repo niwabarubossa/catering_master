4e595bf1fb5e267b9cbead48205d4df91c8abc53
niwabarubossa <niwabarubossa@yahoo.co.jp>
2020-08-22 19:06:16 +0900

    viewcontroller file ccreate

---
+++ catering_for_privatejet/SearchOrAddRateViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SearchOrAddRateViewController.swift
+//  catering_for_privatejet
+//
+//  Created by Ni Ryogo on 2020/08/22.
+//  Copyright © 2020 Ni Ryogo. All rights reserved.
+//
+
+import UIKit
+
+class SearchOrAddRateViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }goToSearchResultPage
+    */
+
+}
