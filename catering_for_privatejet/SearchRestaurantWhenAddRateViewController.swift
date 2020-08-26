56c7d812f4f72a03bd1b006a0ed65ef7cd0f5164
niwabarubossa <niwabarubossa@yahoo.co.jp>
2020-08-26 20:51:13 +0900

    不要なコメントアウト削除

--- catering_for_privatejet/SearchRestaurantWhenAddRateViewController.swift
+++ catering_for_privatejet/SearchRestaurantWhenAddRateViewController.swift
@@ -12,23 +12,11 @@ class SearchRestaurantWhenAddRateViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
     @IBAction func searchButtonTapped(_ sender: Any) {
         self.performSegue(withIdentifier: "goToAddRatePage", sender: nil)
         //did select使うやつ
     }
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
